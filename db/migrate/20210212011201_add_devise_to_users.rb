5167232c520da56acd8a9d56354893dbe33217cc
joshfng <121190566+joshfng@users.noreply.github.com>
2021-02-12 20:27:44 -0500

    Foreman & Rubocop the repo

--- db/migrate/20210212011201_add_devise_to_users.rb
+++ db/migrate/20210212011201_add_devise_to_users.rb
@@ -4,9 +4,9 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :email_ciphertext, null: false, default: ""
-      t.string :email_bidx, null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email_ciphertext, null: false, default: ''
+      t.string :email_bidx, null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
       t.string   :reset_password_token
@@ -23,8 +23,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
       t.string   :confirmation_token
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
-      t.string :unconfirmed_email_ciphertext, null: false, default: ""
-      t.string :unconfirmed_email_bidx, null: false, default: ""
+      t.string :unconfirmed_email_ciphertext, null: false, default: ''
+      t.string :unconfirmed_email_bidx, null: false, default: ''
 
       ## Lockable
       t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
