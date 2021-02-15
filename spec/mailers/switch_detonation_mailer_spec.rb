bda360eb727d01ff3ccecba5ec0d02bfa02be9c0
Josh Frye <josh@joshfrye.com>
2021-02-15 18:21:42 -0500

    Mailer specs

--- spec/mailers/switch_detonation_mailer_spec.rb
+++ spec/mailers/switch_detonation_mailer_spec.rb
@@ -3,5 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe SwitchDetonationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'send_switch_data_to_switch_destination' do
+    let(:switch_destination) { create(:switch_destination) }
+
+    let(:mail) do
+      described_class
+        .with(switch_destination: switch_destination)
+        .send_switch_data_to_switch_destination
+    end
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Someone has sent you sensitive data')
+      expect(mail.to).to eq([switch_destination.switch_destination_address])
+      expect(mail.from).to eq(['noreply@detonateapp.com'])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to include('Someone has sent you sensitive data with Detonate')
+    end
+  end
 end
