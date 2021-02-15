6e2fb421417a36b8e5e4761ac111fb8c22fd17a6
joshfng <121190566+joshfng@users.noreply.github.com>
2021-02-15 18:21:42 -0500

    Mailer specs

--- app/mailers/heartbeat_mailer.rb
+++ app/mailers/heartbeat_mailer.rb
@@ -5,6 +5,9 @@ class HeartbeatMailer < ApplicationMailer
     @heartbeat = params[:heartbeat]
     @url = heartbeat_receive_url(@heartbeat.id)
 
-    mail(to: @heartbeat.heartbeat_destination.heartbeat_destination_address, subject: 'Heartbeat Attempt')
+    mail(
+      to: @heartbeat.heartbeat_destination.heartbeat_destination_address,
+      subject: 'Heartbeat Attempt'
+    )
   end
 end
--- spec/mailers/heartbeat_mailer_spec.rb
+++ spec/mailers/heartbeat_mailer_spec.rb
@@ -3,5 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe HeartbeatMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'send_heartbeat' do
+    let(:heartbeat) { create(:heartbeat) }
+
+    let(:mail) do
+      described_class
+        .with(heartbeat: heartbeat)
+        .send_heartbeat
+    end
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Heartbeat Attempt')
+      expect(mail.to).to eq([heartbeat.heartbeat_destination.heartbeat_destination_address])
+      expect(mail.from).to eq(['noreply@detonateapp.com'])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to include('This is an attempt to check your heartbeat for your switch named')
+    end
+  end
 end
