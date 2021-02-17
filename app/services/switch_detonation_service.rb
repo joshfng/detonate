30578829bd8db862028d7bcd11d88ff9c5744f1b
Josh Frye <josh@joshfrye.com>
2021-02-17 10:43:29 -0500

    Cleanup

--- app/services/switch_detonation_service.rb
+++ app/services/switch_detonation_service.rb
@@ -7,25 +7,29 @@ class SwitchDetonationService < ApplicationService
     @switch = switch
     @force = force
 
-    detonate_switch
+    detonate_switches
   end
 
-  def detonate_switch
+  def detonate_switches
     raise "Tried to detonate switch #{switch.id} prematurely" if switch.alive? && !force
     raise "Tried to detonate switch #{switch.id} that has already detonated" if switch.detonated?
 
     switch.switch_destinations.find_each do |switch_destination|
-      case switch_destination.switch_destination_type
-      when 'email'
-        SwitchDetonationMailer
-          .with(switch_destination: switch_destination)
-          .send_switch_data_to_switch_destination
-          .deliver_later
-      end
-
-      switch_destination.update(switch_destination_notified: true)
+      detonate_switch(switch_destination)
     end
 
     switch.update(detonated: true)
+  end
+
+  def detonate_switch(switch_destination)
+    case switch_destination.switch_destination_type
+    when 'email'
+      SwitchDetonationMailer
+        .with(switch_destination: switch_destination)
+        .send_switch_data_to_switch_destination
+        .deliver_later
+    end
+
+    switch_destination.update(switch_destination_notified: true)
   end
 end
