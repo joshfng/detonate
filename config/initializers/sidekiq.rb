5c4354dcf8483d5e749719fa92085723f77b171d
Josh Frye <josh@joshfrye.com>
2021-02-18 20:40:18 -0500

    Release stuff

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,17 +1,9 @@
 # frozen_string_literal: true
 
-schedule_file = 'config/schedule.yml'
-
-if File.exist?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash(YAML.load_file(schedule_file))
-end
-
 Sidekiq.configure_server do |config|
-  config.redis = { namespace: "detonate_sidekiq_#{Rails.env}" }
-end
+  schedule_file = "config/schedule.yml"
 
-Sidekiq.configure_client do |config|
-  config.redis = { namespace: "detonate_sidekiq_#{Rails.env}" }
+  if File.exist?(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  end
 end
-
-Sidekiq::Extensions.enable_delay!
