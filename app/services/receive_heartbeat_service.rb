41e351ffb4828b9ccbe7cb30dfde8c308e7e1874
Josh Frye <josh@joshfrye.com>
2021-02-18 21:48:14 -0500

    Don't bail if heartbeat is dead. Only detonated

--- app/services/receive_heartbeat_service.rb
+++ app/services/receive_heartbeat_service.rb
@@ -3,7 +3,6 @@
 class ReceiveHeartbeatService < ApplicationService
   def perform(heartbeat)
     return false if heartbeat.heartbeat_confirmed?
-    return false if heartbeat.switch.dead?
     return false if heartbeat.switch.detonated?
 
     heartbeat.switch.heartbeats.update_all(heartbeat_confirmed: true) # rubocop:disable Rails/SkipsModelValidations
