d6b31b15318fd4e209edc7edb3a4a3b4397b8fb8
joshfng <121190566+joshfng@users.noreply.github.com>
2021-02-18 19:48:14 -0500

    Update prod host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -121,10 +121,10 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_controller.default_url_options = {
-    host: 'detonateapp.com'
+    host: 'www.detonateapp.com'
   }
 
   config.action_mailer.default_url_options = {
-    host: 'detonateapp.com'
+    host: 'www.detonateapp.com'
   }
 end
