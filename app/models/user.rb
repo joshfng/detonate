4354fe26d885b0d5a1722d6726dcbfb77ef7ac92
joshfng <121190566+joshfng@users.noreply.github.com>
2021-03-11 19:56:24 -0500

    Linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :database_authenticatable, #, :registerable,
+  devise :database_authenticatable, # , :registerable,
          :recoverable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
 
