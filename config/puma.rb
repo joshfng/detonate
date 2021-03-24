f1b673f886f1eb3be496f8545d70cd92a7142c2c
Josh Frye <josh@joshfrye.com>
2021-03-23 22:15:08 -0400

    PrerenderChrome tests

--- config/puma.rb
+++ config/puma.rb
@@ -10,6 +10,8 @@ max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 threads min_threads_count, max_threads_count
 
+# workers(10)
+
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #
