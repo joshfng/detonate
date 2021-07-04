2deb551065afdf029e92d4de7d4680cd10d1008c
joshfng <121190566+joshfng@users.noreply.github.com>
2021-07-04 12:58:41 -0400

    Ruby 3

---
+++ app/controllers/avo/users_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Avo
+  class UsersController < Avo::ResourcesController
+  end
+end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,5 +3,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
