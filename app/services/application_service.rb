58d12a6c403c265bde9e013f280627d8ac8d2bb8
joshfng <121190566+joshfng@users.noreply.github.com>
2021-07-11 02:48:25 -0400

    Use new delegation syntax

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationService
-  # include Rails.application.routes.url_helpers
-
-  def self.perform(args)
-    new.perform(**args)
+  def self.perform(...)
+    new.perform(...)
   end
-
-  # def default_url_options
-  #   Rails.application.config.action_mailer.default_url_options
-  # end
 end
